10e562ac3755417666541275e1341bd1f65e95ff
MartinaDR1 <derosemartina933@gmail.com>
2023-03-10 18:06:34 +0100

    add blocks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,37 @@
 //Richiamo la lista
-const ulElement = document.querySelector('ul');
+const numElement = document.getElementById('number');
 
 for (let i = 1; i<= 100; i++){
-    console.log(i);
+   
+  switch (true) {
+    // numeri divisibili per 3 e 5
 
-    const liElement = `<li class = "box list-unstyled border">${i}</li>`;
+    case i % 3 == 0 && i % 5 === 0:
+      console.log(i, "FizzBuzz");
+      numElement.innerHTML += `<div class="box border bg-danger">FizzBuzz</div>`;
+    break;
 
-    // Stampo in pagina gli elementi della lista
+    // numeri divisibili per 3
 
-    ulElement.insertAdjacentHTML ('beforebegin', liElement);
+    case i % 3 == 0:
+      console.log(i, "Fizz");
+      numElement.innerHTML += `<div class="box border bg_mod3">Fizz</div>`;
+    break;
+
+    // numeri divisibili per 5
+
+    case i % 5 == 0:
+      console.log(i, "Buzz");
+      numElement.innerHTML += `<div class="box border bg-warning">Buzz</div>`;
+    break;
+
+    // tutti i numeri da 1 a 100
+
+    default:
+      console.log("Default", i);
+      numElement.innerHTML += `<div class="box border">${i}</div>`;
+    break;
+  }
+
+    
 }
