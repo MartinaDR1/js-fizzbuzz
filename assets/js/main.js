81903ab6b6571d7281550b9e11ea12afb803621d
MartinaDR1 <derosemartina933@gmail.com>
2023-03-10 21:36:32 +0100

    add details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//Richiamo la lista
+//Richiamo l'elemento
 const numElement = document.getElementById('number');
 
 for (let i = 1; i<= 100; i++){
@@ -7,29 +7,25 @@ for (let i = 1; i<= 100; i++){
     // numeri divisibili per 3 e 5
 
     case i % 3 == 0 && i % 5 === 0:
-      console.log(i, "FizzBuzz");
-      numElement.innerHTML += `<div class="box border bg-danger">FizzBuzz</div>`;
+      numElement.innerHTML += `<div class="box bg-danger">FizzBuzz</div>`;
     break;
 
     // numeri divisibili per 3
 
     case i % 3 == 0:
-      console.log(i, "Fizz");
-      numElement.innerHTML += `<div class="box border bg_mod3">Fizz</div>`;
+      numElement.innerHTML += `<div class="box bg_mod3">Fizz</div>`;
     break;
 
     // numeri divisibili per 5
 
     case i % 5 == 0:
-      console.log(i, "Buzz");
-      numElement.innerHTML += `<div class="box border bg-warning">Buzz</div>`;
+      numElement.innerHTML += `<div class="box bg-warning">Buzz</div>`;
     break;
 
     // tutti i numeri da 1 a 100
 
     default:
-      console.log("Default", i);
-      numElement.innerHTML += `<div class="box border">${i}</div>`;
+      numElement.innerHTML += `<div class="box">${i}</div>`;
     break;
   }
 
